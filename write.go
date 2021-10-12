a759a59f107338dab30d057acf5fd50330aa5428
maksialexandr <35528125+maksialexandr@users.noreply.github.com>
2021-10-12 13:51:08 +0500

    Update write.go

--- write.go
+++ write.go
@@ -55,7 +55,6 @@ func (f *methodFrame) write(w io.Writer) (err error) {
 //
 // Payload is empty
 func (f *heartbeatFrame) write(w io.Writer) (err error) {
-	print("111111111111111111111111111111")
 	return writeFrame(w, frameHeartbeat, f.ChannelId, []byte{})
 }
 
